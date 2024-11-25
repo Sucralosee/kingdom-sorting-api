628d316287cb9a33500ccefe42f35f90df2bdfc6
Ben Louis <142682578+Sucralosee@users.noreply.github.com>
2024-11-24 20:11:54 -0800

    cmon

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,21 @@
 "use client";
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Countries from './components/Countries';
 import Details from './components/Details';
+import ClientOnly from './components/ClientOnly'; // Adjust path as needed
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Countries />} />
-        <Route path="countries" element={<Countries />}>
-          <Route path=":cca2" element={<Details />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ClientOnly>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Countries />} />
+          <Route path="countries" element={<Countries />}>
+            <Route path=":cca2" element={<Details />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ClientOnly>
   );
 }
